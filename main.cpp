821a072d4428ead03368da51b1d2284e62ac922f
George Uy De Ong <guyde001@hammer.cs.ucr.edu>
2016-02-05 00:48:12 -0800

    added gethostname(). Change the prompt to make it shorter and cleaner. Both extra credit and prompt are done

--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 #include <stdlib.h>
+#include <unistd.h>
 
 using namespace std;
 
+//Prompt which will print [USERNAME]@[HOSTNAME]$
 void prompt()
 {
+	string username = getlogin();		
+	char hostname[1024];		//Created a 1024 elemetn hostname
+	hostname[1023] = '\0';		
+	
+	//Checks if getlogin() returns 0 when user is NULL else prints USERNAME
+	getlogin() == 0 ? cout << "user" : cout << username;
+	
+	//Checks if gethosname() returns -1 when host is NULL else prints HOSTNAME	
+	gethostname(hostname, 1023) < 0 ? cout << "@host" : cout << "@" << hostname;
 	cout << "$ ";
 }
 
 int main()
 {
 	string line;
+	
 	while(1)
-	{
-		prompt();
-	 	getline(cin, line);
-		if(line == "exit")
-		 {
-			exit(0);
-		 }
-	}	
+	 {
+	  prompt();		//prints [USERNAME]@[HOSTNAME]$	
+	  getline(cin, line);	
+	  
+	  if(line == "exit")	//EXIT program if user types "exit"
+	    {
+	      exit(0);
+	    }
+	 }	
 }
 
