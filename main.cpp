8085b19729e76d79e8debf6a913dac9451a0e7aa
George Uy De Ong <guyde001@hammer.cs.ucr.edu>
2016-02-07 21:36:56 -0800

    combined my partner's code with mine and fixed the errors. NOW IT IS WORKING

--- main.cpp
+++ main.cpp
@@ -19,7 +19,15 @@ void prompt()
 	getlogin() == 0 ? cout << "user" : cout << username;
 	
 	//Checks if gethosname() returns -1 when host is NULL else prints HOSTNAME	
-	gethostname(hostname, 1023) < 0 ? cout << "@host" : cout << "@" << hostname;
+	gethostname(hostname, 1023) < 0 ? cout << "@host" : cout << "@" << hostname << " ";
+	
+	char path[1024];
+	char* checkPath = getcwd(path, 1023);
+	if(checkPath == NULL)
+	{
+		perror("Error in getcwd");
+  	}
+	cout << path;
 	cout << "$ ";
 }
 
@@ -61,15 +69,15 @@ int Commands::connectP(string com, int andCheck, int orCheck) // for parsing con
 		locAnd = 0;
 	}
 	
-	if(locOr != 0)
+	if(locAnd != 0)
+	{
+		orCheck = connectP(com.substr(0, locAnd - com.c_str()), orCheck, andCheck); //command succeeded
+		orCheck != 1 ? connectP(locAnd + 2, 2, 0) : connectP(locAnd + 2, 1, 0);
+	}
+	else if(locOr != 0)
 	{
 		andCheck = connectP(com.substr(0, locOr - com.c_str()), orCheck, andCheck);
 		andCheck != 1 ? connectP(locOr + 2, 0, 1) : connectP(locOr + 2, 0, 2);
-	}
-	else if (locAnd != 0)
-	{
-		orCheck = connectP(com.substr(0, locAnd - com.c_str()), orCheck, andCheck);
-		orCheck != 1 ? connectP(locAnd + 2, 2, 0) : connectP(locAnd + 2, 1, 0);
 	}
 	else
 	{
@@ -88,176 +96,205 @@ int Commands::connectP(string com, int andCheck, int orCheck) // for parsing con
 
 bool Commands::execforcommand(string& com)
 {
-	if(com.compare("exit") == 0)
+	if (com.compare("exit") == 0)	
 	{
 		exit(0);
 	}
-	
 	int location = com.find("cd");
-	if(location > -1)
+	if (location > -1 )	
 	{
 		string cdir;
-		char * strings;
-		string homebase;
-		const char * args;
+		char* strings;
 		char path[2048];
-		string t = "";
-
-		if(com.size() > 2)
+		const char* args;
+		string homebase;
+		string temps = "";
+		if (com.size() > 2)
 		{
-			t = com.substr(location + 3);
-			args = t.c_str();
+			temps = com.substr(location + 3);
+			args = temps.c_str();
 		}
-
-		if(t.size() == 0)
+		if (temps.size() == 0)	
 		{
-			char * directory = getenv("HOME");
+			char * cdir = getenv("HOME");
 			homebase = "";
-			if(directory == NULL)
+			if(cdir != NULL)
 			{
-			 perror("Error in getenv()");
+				homebase += cdir;
 			}
-			else
+			else	
 			{
-			 homebase = homebase + directory;
+			    perror("error in getenv");
 			}
-			char * check = (getcwd(path, 2057));
-
-			if(check != '\0')
+			char * check = getcwd(path, 2047);
+			if(check != '\0')	
 			{
 				int checkcheck = setenv("OLDPWD", path, 1);
-				if(checkcheck <= -1)
+				if (checkcheck <= -1)	
 				{
-					perror("Error in setenv()");
+					perror("error in setenv");
 				}
 			}
-			else
+			else 
 			{
 				perror("Error in getcwd()");
+			}		
+			int tricheck = chdir(homebase.c_str());
+			if(tricheck <= -1)	
+			{
+				perror("error in chdir");
 			}
-			int tricheck = chdir(com.c_str());
-			if (tricheck <= -1 )
-        		{
-         			perror("Error in chdir()");
-        		}
-       			return true; 
+			return true;
 		}
-		else if(strcmp(args, "-") == 0)
+		else if (strcmp(args, "-") == 0) 
 		{
 			cdir = "";
 			strings = getenv("OLDPWD");
-			char * check = getcwd(path, 2047);
-
-			if(strings != '\0')
+			if (strings != '\0')
 			{
 				cdir += strings;
 			}
 			else
 			{
-				perror("Error in getenv()");
+				perror("error in getenv");
 			}
-			if(check != '\0')
+			char* check = getcwd(path, 2047);
+			if(check != '\0')	
 			{
 				int checkcheck = setenv("OLDPWD", path, 1);
-			
-				if(checkcheck <= -1)
+				if (checkcheck <= -1)	
 				{
-					perror("Error in setenv");
+					perror("error in setenv");
 				}
 			}
-			else
+			else 
 			{
 				perror("Error in getcwd()");
-			}
-			
+			}		
 			int tricheck = chdir(cdir.c_str());
-			if(tricheck <= -1) 
+			if (tricheck <= -1)	
 			{
-				perror("Error in chdir()");
-			} 
+				perror("error in chdir");
+			}
 			return true;
-		 }
-		char * check = getcwd(path, 2047);
-		if(check != '\0')
+		
+		}
+
+		else if (strcmp(args, "~") == 0)	
+		{
+			char* cdir = getenv("HOME");
+			homebase = "";
+			if (cdir != NULL)	
+			{
+				homebase += cdir;
+			}
+			else 
+			{
+				perror("error in getenv");
+			}
+			char* check = getcwd(path, 2047);
+			if (check != '\0')	
+			{
+				int checkcheck = setenv("OLDPWD", path, 1);
+				if (checkcheck <= -1)	
+				{
+					perror("error in setenv");
+				}
+			}
+			else 
+			{
+		        perror("Error in getcwd()");
+			}		
+			int tricheck = chdir(homebase.c_str());
+			if(tricheck <= -1)	
+			{
+				perror("error in chdir");
+			}
+			return true;
+	
+		}
+		char* check = getcwd(path, 2047);
+		if (check != '\0')	
 		{
 			int checkcheck = setenv("OLDPWD", path, 1);
-			if(checkcheck <= -1)
+			if (checkcheck <= -1) 
 			{
-				perror("Error in setenv()");
+				perror("error in setenv");
 			}
 		}
-		else
+		else 
 		{
 			perror("Error in getcwd()");
 		}
 		int tricheck = chdir(args);
-		if(tricheck <= -1)
+		if (tricheck <= -1) 
 		{
-			perror("Error in chdir()");	
+			perror("error in chdir");
 		}
+		
 		return true;
-	}	
-		int pro1 = fork();	//Forking
-		if(pro1 <= -1)
-		{
-			perror("Error in creating fork");
-			return false;
-		}
-		if(signal(SIGINT, Commands::sigHandle) == SIG_ERR)
-		{
-			perror("Error in signal");
-		}
-		else if(pro1 == 0)
-		{
-			char * arg[2048];
-			getarg(com, arg);
-			int res = execvp(*arg, arg);
-			
-			if(res <= -1)
-			{
-				perror("Error in executing command line 202");
-				exit(1);
-			}	
-		}
-		else
-		{
-			int childWait = waitpid(pro1, &childWait, 0);
-			if(childWait <= -1)
-			{
-				perror("Waiting failed");
-			}
-			if(childWait == 0)
-			{
-				return true;
-			}
+	}
+	int pro = fork();
+	if (pro <= -1)	
+	{
+		perror("ERROR IN CREATING FORK");
 		return false;
+	}
+	if (signal(SIGINT, sigHandle) == SIG_ERR)
+	{
+		perror ("ERROR IN SIGNAL");
+	}
+	else if (pro == 0)	
+	{
+		char * arg[2048];
+		getarg(com, arg);
+
+		int res = execvp(*arg, arg);
+		if (res <= -1)	
+		{
+			perror("ERROR IN EXECUTING COMMAND LINE 202");
+			exit(1);
 		}
-		return false;
+	}
+	else 
+	{
+		int childWait = waitpid(pro, &childWait, 0);
+		if (childWait <= -1)
+		{
+			perror("WAITING FOR CHILD FAILED");
+		}
+		if(childWait == 0)
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
 void Commands::getarg(string& com, char* arrayargs[])
 {
-    char * WOwhitespace = strtok(&com[0], "\t"); //to remove whitespace 
-    vector <string> temps;
+    char* WOwhitespace = strtok(&com[0], " \t");
+    vector<string> temps;
     while (WOwhitespace)
     {
         temps.push_back(WOwhitespace);
-        WOwhitespace = strtok(0, "\t");
+        WOwhitespace = strtok(0, " \t");
     }
-    for (int i = 0; i < temps.size(); ++i)
+    for (unsigned i = 0; i < temps.size(); ++i)
     {
-        string &toSwap = temps.at(i);
-        arrayargs[i] = &toSwap[0];
+        string& toswap = temps.at(i);
+        arrayargs[i] = &toswap[0];
     }
     arrayargs[temps.size()] = '\0';
 }
+
 void sigHandle(int signal)
 {
 
 }
 int main(int argc, char * argv[])
 {
-	string line;
+	string commLine;
 	Commands * c = new Commands();	
 	while(1)
 	 {
@@ -266,13 +303,13 @@ int main(int argc, char * argv[])
 	  {
 		perror("Error in signal");
 	  }
-	  getline(cin, line);	
+	  getline(cin, commLine);	
 
-	  if(line == "exit")	//EXIT program if user types "exit"
+	  if(commLine == "exit")	//EXIT program if user types "exit"
 	    {
 	      exit(0);
 	    }
-	    c->commandP(line);
+	    c->commandP(commLine);
 	 }	
 	return 0;
 }
