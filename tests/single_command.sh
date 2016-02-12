339f4cddb90a42e3a302416c8fd6efc2a5cf36f5
Jamie Alaniz <jalan003@hammer.cs.ucr.edu>
2016-02-11 22:27:54 -0800

    Added single_command.sh

---
+++ tests/single_command.sh
@@ -0,0 +1,174 @@
+Script started on Thu 11 Feb 2016 10:18:08 PM PST
+]0;jalan003@hammer:~/CS100/Assignment2/rshell/tests[?1034h[jalan003@hammer tests]$ cd ..
+]0;jalan003@hammer:~/CS100/Assignment2/rshell[jalan003@hammer rshell]$ cd scr[K[Krc
+]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ make
+g++  -W -Werror -pedantic -ansi main.cpp
+]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ ./a.out
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ ls
+a.out  main.cpp  makefile
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ cd ..
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ ls
+a.out  LICENSE	makefile  README.md  src  tests
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ -s  ls -a
+.  ..  a.out  .git  LICENSE  .main.cpp.swp  makefile  README.md  src  tests
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ s data e
+Thu Feb 11 22:18:54 PST 2016
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ echo moose
+moose
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ mk  dir temp
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ ls
+a.out  LICENSE	makefile  README.md  src  temp	tests
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ hostname
+hammer.cs.ucr.edu
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ id' 
+uid=41896(jalan003) gid=950(csmajs) groups=950(csmajs)
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ logname
+jalan003
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ pwd
+/home/csmajs/jalan003/CS100/Assignment2/rshell
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ it  time
+Usage: time [-apvV] [-f format] [-o file] [--append] [--verbose]
+       [--portability] [--format=format] [--output=file] [--version]
+       [--help] command [arg...]
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ uname
+Linux
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ wait 2
+ERROR IN EXECUTING COMMAND LINE 202: No such file or directory
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ ls
+a.out  LICENSE	makefile  README.md  src  temp	tests
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ rm -rf temp
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ ls
+a.out  LICENSE	makefile  README.md  src  tests
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ git log
+[?1h=[33mcommit 03ddfa0d27672365a336000c929d0cc5080b8419[m[m
+Author: Jamie Alaniz <jalan003@hammer.cs.ucr.edu>[m
+Date:   Thu Feb 11 21:43:32 2016 -0800[m
+[m
+    Moving things to proper files[m
+[m
+[33mcommit 30b49ce37c61b8dbd02364ebb927caaf722b6b61[m[m
+Author: Jamie Alaniz <jalan003@hammer.cs.ucr.edu>[m
+Date:   Thu Feb 11 21:39:55 2016 -0800[m
+[m
+    Added ls- problem[m
+[m
+[33mcommit 55c79d0a5e907578d5e2df2b47545bb0937ddad2[m[m
+Author: Jamie Alaniz <jalan003@hammer.cs.ucr.edu>[m
+Date:   Wed Feb 10 16:10:04 2016 -0800[m
+[m
+    Fixed connector mixup[m
+[m
+[33mcommit adc76b63159f9bb26ae2662e30849346edcd27c4[m[m
+Author: Jamie Alaniz <jalan003@hammer.cs.ucr.edu>[m
+Date:   Wed Feb 10 12:34:18 2016 -0800[m
+[m
+    Added arrow keys[m
+[m
+[33mcommit 1ccd627b8f149cf612640fc8cf6410695e8c8438[m[m
+Author: Jamie Alaniz <jalan003@hammer.cs.ucr.edu>[m
+Date:   Tue Feb 9 23:02:11 2016 -0800[m
+[m
+    Forgot t on link[m
+[m
+[33mcommit c1b75feb5081c77b530a53609f8bc958ba5aee8f[m[m
+Author: Jamie Alaniz <jalan003@hammer.cs.ucr.edu>[m
+Date:   Tue Feb 9 23:00:13 2016 -0800[m
+[m
+    Still trying to fix code block[m
+[m
+[33mcommit 6c0f8ddc55e52d2429108aeeb74326bc88e3c400[m[m
+Author: Jamie Alaniz <jalan003@hammer.cs.ucr.edu>[m
+Date:   Tue Feb 9 22:56:21 2016 -0800[m
+[m
+    Trying to fix block code[m
+[m
+[33mcommit a4619df43bb982d4a48b60a48062e6b049210557[m[m
+Author: Jamie Alaniz <jalan003@hammer.cs.ucr.edu>[m
+Date:   Tue Feb 9 22:51:07 2016 -0800[m
+[m
+    Added install and bugs[m
+[m
+[33mcommit 9146e2b2d52c71d3013f8eb3ae11ba497acb3c21[m[m
+Author: Jamie Alaniz <jalan003@hammer.cs.ucr.edu>[m
+Date:   Tue Feb 9 15:32:03 2016 -0800[m
+:[K[K[m
+:[K[K    ADDED GNU LICENSE[m
+:[K[K[m
+:[K[K[33mcommit e1108437111ca6ec8390f41a509608375ce7ccdb[m[m
+:[K[KAuthor: Jamie Alaniz <jalan003@hammer.cs.ucr.edu>[m
+:[K[KDate:   Mon Feb 8 15:56:15 2016 -0800[m
+:[K[K[m
+:[K[K    Added makefile[m
+:[K[K[m
+:[K[K[33mcommit 8085b19729e76d79e8debf6a913dac9451a0e7aa[m[m
+:[K[KAuthor: George Uy De Ong <guyde001@hammer.cs.ucr.edu>[m
+:[K[KDate:   Sun Feb 7 21:36:56 2016 -0800[m
+:[K[K[m
+:[K[K    combined my partner's code with mine and fixed the errors. NOW IT IS WORKING[m
+:[K[K[m
+:[K[K[33mcommit cd7880e55e402a2d8fefbc0c72c4533878d0a19d[m[m
+:[K[KAuthor: George Uy De Ong <guyde001@hammer.cs.ucr.edu>[m
+:[K[KDate:   Sat Feb 6 20:29:10 2016 -0800[m
+:[K[K[m
+:[K[K    Finished editing execforcommand and fixing the errors. Program partially works! =D[m
+:[K[K[m
+:[K[K[33mcommit d7e3e1ef6cfea46c527f30c6d7c515f072c0b81e[m[m
+:[K[KAuthor: George Uy De Ong <guyde001@hammer.cs.ucr.edu>[m
+:[K[KDate:   Sat Feb 6 18:48:32 2016 -0800[m
+:[K[K[m
+:[K[K    fixing execforcommand ver. 2[m
+:[K[K[m
+:[K[K[33mcommit 135eba4eb1776aed647552235d84170f42cdaaaf[m[m
+:[K[KAuthor: George Uy De Ong <guyde001@hammer.cs.ucr.edu>[m
+:[K[KDate:   Sat Feb 6 18:33:06 2016 -0800[m
+:[K[K[m
+:[K[K    still fixing execforcommand[m
+:[K[K[m
+:[K[K[33mcommit d77f337e83c179e6bb4e4ad7183c5fa83fcff041[m[m
+:[K[KAuthor: George Uy De Ong <guyde001@hammer.cs.ucr.edu>[m
+:[K[KDate:   Sat Feb 6 18:02:56 2016 -0800[m
+:[K[K[m
+:[K[K    added exectforcommand function. It will NOT compile. INCOMPLETE[m
+:[K[K[m
+:[K[K[33mcommit 2767f0ed3589eb7b7984eea2be5dc65ca4fa3c09[m[m
+:[K[KAuthor: George Uy De Ong <guyde001@hammer.cs.ucr.edu>[m
+:[K[KDate:   Sat Feb 6 03:18:01 2016 -0800[m
+:[K[K[m
+:[K[K    added the connector function to check AND and OR[m
+:[K[K[m
+:[K[K[33mcommit ea4e7b3571733e95394747b7c7473c60b2fad1d5[m[m
+:[K[KAuthor: George Uy De Ong <guyde001@hammer.cs.ucr.edu>[m
+:[K[KDate:   Sat Feb 6 02:47:39 2016 -0800[m
+:[K[K[m
+:[K[K    added class Commands with functions[m
+:[K[K[m
+:[K[K[33mcommit 821a072d4428ead03368da51b1d2284e62ac922f[m[m
+:[K[KAuthor: George Uy De Ong <guyde001@hammer.cs.ucr.edu>[m
+:[K[KDate:   Fri Feb 5 00:48:12 2016 -0800[m
+:[K[K[m
+:[K[K    added gethostname(). Change the prompt to make it shorter and cleaner. Both extra credit and promp[m
+:[K[K[m
+:[K[K[33mcommit 5fbe4d2c616d932b5ad05b44724c9b8f3db06f59[m[m
+:[K[KAuthor: George Uy De Ong <guyde001@hammer.cs.ucr.edu>[m
+:[K[KDate:   Fri Feb 5 00:05:57 2016 -0800[m
+:[K[K[m
+:[K[K    added prompt() and exit()[m
+:[K[K[m
+:[K[K[33mcommit 4c7bed2ff27c94a4f2bc5ce6b00e086fd2343a02[m[m
+:[K[KAuthor: georgeuy2 <georgeuy2@yahoo.com>[m
+:[K[KDate:   Wed Feb 3 15:13:49 2016 -0800[m
+:[K[K[m
+:[K[K    Initial commit[m
+:[K[K[K[7m(END) [27m[K[K[K[7m(END) [27m[K[K[K[7m(END) [27m[K[K[K[7m(END) [27m[K[K[K[7m(END) [27m[K[K[K[7m(END) [27m[K[K[?1l>jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ echo end te      end test               ls-
+ERROR IN EXECUTING COMMAND LINE 202: No such file or directory
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ la  testing invalids
+ERROR IN EXECUTING COMMAND LINE 202: No such file or directory
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ cd ;k
+ERROR IN EXECUTING COMMAND LINE 202: No such file or directory
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003$ ech  p   o "Cant    n't think of anything else to run"
+"Can't think of anything else to run"
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003$ exit 
+]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ exit
+exit
+
+Script done on Thu 11 Feb 2016 10:23:56 PM PST
