9fd856dff73910d0bc87697870634f0ce6409ec4
Jamie Alaniz <jalan003@hammer.cs.ucr.edu>
2016-02-11 23:17:53 -0800

    Added commented_command.sh

---
+++ tests/commented_command.sh
@@ -0,0 +1,44 @@
+Script started on Thu 11 Feb 2016 11:11:06 PM PST
+]0;jalan003@hammer:~/CS100/Assignment2/rshell/tests[?1034h[jalan003@hammer tests]$ cd ..
+]0;jalan003@hammer:~/CS100/Assignment2/rshell[jalan003@hammer rshell]$ cd sr[Kcr[K[Krc
+]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ make
+g++  -W -Werror -pedantic -ansi main.cpp
+]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ ./a.out
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ ls #comment here
+a.out  main.cpp  makefile
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ ls -a #comment       moose
+.  ..  a.out  main.cpp	makefile
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ cd .. #the  esting comments
+error in chdir: No such file or directory
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ cd    cd ..
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ cd  l cd #comments
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003$ cd    cd CS	100 #comment
+error in chdir: No such file or directory
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003$ ech  o testing #comments
+testing
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003$ mkdir       cd CS	100
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100$ cd A	ssignment  2
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2$ mkdir #testing stuff
+mkdir: missing operand
+Try `mkdir --help' for more information.
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2$ ma kdir temp #testing things
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2$ ls #yup
+rshell	temp
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2$ rm -rf #remove that
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2$ ls
+rshell	temp
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2$ rm  cd rshell
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ data e #what's today?
+Thu Feb 11 23:15:16 PST 2016
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ ls    ls    echo that's      today && data               "Tota  day is" && data e #with connectors
+"Today is"
+Thu Feb 11 23:16:02 PST 2016
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ cd #error      ls #error || echo error #should be
+error in chdir: No such file or directory
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ echo d finsihe    ished #d done
+finished
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ exit #end
+]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ exit
+exit
+
+Script done on Thu 11 Feb 2016 11:17:08 PM PST
