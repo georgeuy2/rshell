9a88de3733f4bdf5a3947e8b749a835c9ebf8452
alanizjamie <jalan003@ucr.edu>
2016-02-12 16:07:39 -0800

    Update exit.sh
    updated exit.sh

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,29 +1,5 @@
-Script started on Thu 11 Feb 2016 10:36:59 PM PST
-]0;jalan003@hammer:~/CS100/Assignment2/rshell/tests[?1034h[jalan003@hammer tests]$ cd ..
-]0;jalan003@hammer:~/CS100/Assignment2/rshell[jalan003@hammer rshell]$ cd sc[Krc
-]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ make
-g++  -W -Werror -pedantic -ansi main.cpp
-]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ ./a.out
-jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ echo moose && exit
-]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ ./a.out
-jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ echo      cd .. || exit
-]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ ./a.out[3Pmake
-g++  -W -Werror -pedantic -ansi main.cpp
-]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ make./a.out
-jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ echo || eix  xit
-]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ ./a.out[3Pmake./a.out
-jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ ls- || exit
-]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ ./a.out
-jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ echo   yes; echo no; exit
-yes
-no
-]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ ./a.out[3Pmake./a.out
-jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ echo I'  
-
-jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ ^[[A    echo "I've restrar   arted this prob gram many times" && exit
-]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ ls || exot[K[Kit
-[0m[38;5;34ma.out[0m  main.cpp  makefile
-[m]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ exit
+#!/bin/sh
+ls || exit
+ls && exit
+exit #comment
 exit
-
-Script done on Thu 11 Feb 2016 10:39:39 PM PST
